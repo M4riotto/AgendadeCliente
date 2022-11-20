8a8a4fe4bbd5d00b8f7d7c05ea56139c84f5a157
Mona Lisa <vitormariotto03@gmail.com>
2022-11-19 21:38:53 -0300

    sql

--- database/event.sql
+++ database/event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Nov-2022 às 02:32
+-- Tempo de geração: 20-Nov-2022 às 01:38
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -24,23 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cdslocal`
+-- Estrutura da tabela `cdsagenda`
 --
 
-CREATE TABLE `cdslocal` (
+CREATE TABLE `cdsagenda` (
   `id` int(11) NOT NULL,
-  `endereco` varchar(100) NOT NULL,
-  `dia` varchar(10) NOT NULL,
-  `hora` varchar(10) NOT NULL,
-  `cpf` varchar(20) NOT NULL
+  `dia` varchar(15) NOT NULL,
+  `horario` varchar(10) NOT NULL,
+  `comquem` varchar(100) NOT NULL,
+  `assunto` varchar(200) NOT NULL,
+  `topico` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `cdslocal`
---
-
-INSERT INTO `cdslocal` (`id`, `endereco`, `dia`, `hora`, `cpf`) VALUES
-(15, 'Rua AntÃ´nio pereira garcia, 117', '10-01-2023', '09:00HS', '490.401.288-71');
 
 -- --------------------------------------------------------
 
@@ -50,27 +44,20 @@ INSERT INTO `cdslocal` (`id`, `endereco`, `dia`, `hora`, `cpf`) VALUES
 
 CREATE TABLE `eventos` (
   `ID` int(11) NOT NULL,
-  `titulo` varchar(30) NOT NULL,
-  `descricao` varchar(200) NOT NULL,
-  `cover` varchar(500) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `numero` varchar(10) NOT NULL,
   `cpf` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `eventos`
---
-
-INSERT INTO `eventos` (`ID`, `titulo`, `descricao`, `cover`, `cpf`) VALUES
-(43, 'Evento presencial', ' O excÃªntrico magizoologista Newt Scamander (Eddie Redmayne) chega Ã  cidade de Nova York levando com muito zelo sua preciosa maleta, um objeto mÃ¡gico onde ele carrega fantÃ¡sticos animais do mundo ', 'https://www.clickriomafra.com.br/emacite/wp-content/uploads/2016/12/Animais-Fant%C3%A1sticos-e-onde-Habitam.jpg', '490.401.288-71');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `cdslocal`
+-- Índices para tabela `cdsagenda`
 --
-ALTER TABLE `cdslocal`
+ALTER TABLE `cdsagenda`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,16 +71,16 @@ ALTER TABLE `eventos`
 --
 
 --
--- AUTO_INCREMENT de tabela `cdslocal`
+-- AUTO_INCREMENT de tabela `cdsagenda`
 --
-ALTER TABLE `cdslocal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+ALTER TABLE `cdsagenda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de tabela `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
